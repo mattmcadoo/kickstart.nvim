bc400b40bf03f017b14d7f80798e0d80160458d4
Matt McAdoo <git@mattmcadoo.com>
2023-12-24 14:41:41 -0600

    add DAP debugging plugins

--- lua/custom/plugins/init.lua
+++ lua/custom/plugins/init.lua
@@ -2,4 +2,18 @@
 --  I promise not to create any merge conflicts in this directory :)
 --
 -- See the kickstart.nvim README for more information
+require('lazy').setup({
+	'mfussenegger/nvim-dap',
+	'jbyuki/one-small-step-for-vimkind',
+	'mfussenegger/nvim-dap-python',
+	'theHamsta/nvim-dap-virtual-text',
+}, {})
+
+local dap = require("dap")
+dap.adapters.gdb = {
+	type = "executable",
+	command = "gdb",
+	args = { "-i", "dap" }
+}
+
 return {}
