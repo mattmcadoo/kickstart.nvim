2f4cef6894385e6a7edb942ee37c72a10c3331d8
Matt McAdoo <git@mattmcadoo.com>
2024-02-07 22:34:38 -0600

    add tab space to 4 and convert tab to spaces

--- init.lua
+++ init.lua
@@ -317,6 +317,10 @@ vim.wo.relativenumber = true
 -- Enable mouse mode
 vim.o.mouse = 'a'
 
+vim.o.tabstop = 4
+vim.o.shiftwidth = 4
+vim.o.expandtab = true
+
 -- Sync clipboard between OS and Neovim.
 --  Remove this option if you want your OS clipboard to remain independent.
 --  See `:help 'clipboard'`
